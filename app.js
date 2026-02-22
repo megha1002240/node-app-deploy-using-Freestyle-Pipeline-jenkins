aecc7576afcd9eb276bbc8af5b760f367e2e5910
megha1002240 <meghapatil290510@gmail.com>
2026-02-22 12:50:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Node.js App Deployed via Jenkins Freestyle Pipeline 🚀");
+  res.send("Node App Deployed by Jenkins 🚀");
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
+
+// IMPORTANT: bind to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
